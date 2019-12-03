32308ae767ba32e0742d19b5d2dd188ac52dc68f
Alessandro <soralex008@gmail.com>
2019-12-03 18:53:50 +0100

    Insert main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import telepot
+token = '1055118672:AAEpSCfVmv242kDU9F7Bwb3gKr5dXVToRHg'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+print ("\n")
+print (TelegramBot.getUpdates())
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    name = msg["from"]["first_name"]
+    if content_type == 'text':
+        bot.sendMessage(chat_id, 'ciao ' + name)
+
+bot = telepot.Bot(token)
+bot.message_loop(on_chat_message)
+
+print ("Listening ...")
+
+import time
+while 1:
+    time.sleep(10)
